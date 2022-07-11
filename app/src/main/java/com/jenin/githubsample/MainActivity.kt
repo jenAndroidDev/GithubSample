55c597732a357bce1f05ec518e9483c1284de759
jeninjoseph <jenin@pepul.com>
2022-07-11 10:31:20 +0530

    new jenin branch

--- app/src/main/java/com/jenin/githubsample/MainActivity.kt
+++ app/src/main/java/com/jenin/githubsample/MainActivity.kt
@@ -7,9 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        println("This is a sample second comit")
-        println("This is an Experimental commit")
-        println("This is a new line commit")
+        println("This is jenin line")
     }
 }
